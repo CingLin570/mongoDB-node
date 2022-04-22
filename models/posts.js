001ee23873fc80281246e2e7b1a2c5dbcc204db5
謦麟 <asd28618371@gmail.com>
2022-04-22 17:47:20 +0800

    新增posts model

---
+++ models/posts.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const postSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: [true, "貼文姓名未填寫"]
+    },
+    content: {
+      type: String,
+      required: [true, "內容未填寫"]
+    },
+    image: {
+      type: String,
+      default: ""
+    },
+    createAt: {
+      type: Date,
+      default: Date.now,
+      select: false,
+    },
+    likes: {
+      type: Number,
+      default: 0
+    }
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
